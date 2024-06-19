d0c56c779e92d6bf1aeeea1a5cfa430bace38f8b
abril <abicelaya@gmail.com>
2024-06-19 10:24:21 +0200

    maquetar componente Card de forma responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import Card from "./componentes/Card/Card.jsx";
+import TimeManagmentImage from "./img/time_managment.svg";
+import ProgrammingImage from "./img/programming.svg";
+import MeditationImage from "./img/meditation.svg";
 
 import "./App.css";
 
@@ -14,33 +17,33 @@ function App() {
       titulo: "Dedica moltes hores",
       descripcion:
         "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
-      bgColor: "#FFFFFF",
-      imagen: "",
+      bgColor: "#0097A7",
+      imagen: TimeManagmentImage,
     },
     {
       titulo: "Programa projectes propis",
       descripcion:
         "Més val 10 hores traballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
-      bgColor: "#FFFFFF",
-      imagen: "",
+      bgColor: "#D5D8DC",
+      imagen: ProgrammingImage,
     },
     {
       titulo: "Procura descansar",
       descripcion:
         "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs la teva concentració i consolidaràs el teu aprenentatge.",
-      bgColor: "#FFFFFF",
-      imagen: "",
+      bgColor: "#FFD54F",
+      imagen: MeditationImage,
     },
   ];
 
   return (
     <>
-      <div>
+      <div className="d-flex justify-content-center align-items-center vh-100">
         <Card
+          imagen={tutorialData[step].imagen}
           titulo={tutorialData[step].titulo}
           descripcion={tutorialData[step].descripcion}
           bgColor={tutorialData[step].bgColor}
-          imagen={tutorialData[step].imagen}
           irAlSiguientePaso={nextStep}
         />
       </div>
--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -1,3 +1,5 @@
+import "./Card.css";
+
 export default function Card({
   titulo,
   descripcion,
@@ -7,11 +9,21 @@ export default function Card({
 }) {
   return (
     <div className="card" style={{ backgroundColor: bgColor }}>
-      <h1>{titulo}</h1>
-      <p>{descripcion}</p>
-      {imagen && <img src={imagen} alt={titulo} />}
-
-      <button onClick={irAlSiguientePaso}>Següen</button>
+      {imagen && (
+        <img src={imagen} className="card-img-top imagen" alt={titulo} />
+      )}
+      <div className="card-body">
+        <h5 className="card-title">{titulo}</h5>
+        <p className="card-text">{descripcion}</p>
+        <div id="boton">
+          <button
+            className="btn btn-dark btn-circle btn-icon"
+            onClick={irAlSiguientePaso}
+          >
+            <i className="bi bi-arrow-right"></i>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
